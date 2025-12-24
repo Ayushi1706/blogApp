7052707bf85d7e85343fb2d90af3752d84835df1
Ayushi Singh <1032241223@tcetmumbai.in>
2025-12-24 20:10:56 +0530

    Update commentController.js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,41 +1,26 @@
+const Post = require("../models/postModel");
+const Comment = require("../models/commentModel");
+
 exports.createComment = async (req, res) => {
-    try {
-        const { post, user, body } = req.body;
-
-        if (!post || !user || !body) {
-            return res.status(400).json({
-                success: false,
-                message: "post, user and body are required"
-            });
-        }
-
-        const existingPost = await Post.findById(post);
-        if (!existingPost) {
-            return res.status(404).json({
-                success: false,
-                message: "Post not found"
-            });
-        }
-
-        const comment = new Comment({ post, user, body });
-        const savedComment = await comment.save();
-
-        const updatedPost = await Post.findByIdAndUpdate(
-            post,
-            { $push: { comments: savedComment._id } },
-            { new: true }
-        ).populate("comments");
-
-        return res.status(201).json({
-            success: true,
-            post: updatedPost
+    try{
+        const {post, user, body} = req.body;
+        const comment = new Comment({
+            post,user,body
         });
 
-    } catch (error) {
-        console.error("Create Comment Error:", error);
+        const savedComment = await comment.save();
+        const udpatedPost = await Post.findByIdAndUpdate(post, {$push: {comments: savedComment._id} }, {new: true}  )
+                            .populate("comments")
+                            .exec();
+
+        res.json({
+            post: udpatedPost,
+        });
+
+    }
+    catch(error) {
         return res.status(500).json({
-            success: false,
-            error: error.message
+            error: "Error While Creating comment" ,
         });
     }
 };
